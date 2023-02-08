de02482f114e91b95e727fbcdda77cc9e3bc1f88
Marilisk <6868221@gmail.com>
2023-02-08 16:31:24 +0300

    hu

--- src/Components/Favourites/FavoriteProductCard/FavoriteProductCard.jsx
+++ src/Components/Favourites/FavoriteProductCard/FavoriteProductCard.jsx
@@ -33,7 +33,7 @@ export const FavoriteProductCard = ({ id, removeFromFavorites, authIsLoading, in
     return <div className={c.wrap} onClick={() => dispatch(setCurrentProd(product))} >
 
         <NavLink to={`/product/${product._id}`}>
-            <img src={product.imageUrl?.main ? `http://localhost:4444${product.imageUrl.main}` : defaultGlasses} alt='' />
+            <img src={product.imageUrl?.main ? `https://backend-optics-without-packlo.onrender.com${product.imageUrl.main}` : defaultGlasses} alt='' />
             <div className={c.price}>
                 {price}
             </div>
