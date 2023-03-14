f1d38368aea6a3ac25ee21b401ec99d144c9dcc8
Marilisk <6868221@gmail.com>
2023-03-14 17:35:04 +0300

    lenses

--- src/Components/Header/CartBtn/CartBtn.tsx
+++ src/Components/Header/CartBtn/CartBtn.tsx
@@ -1,31 +1,19 @@
 import { NavLink } from 'react-router-dom';
-//import c from './../Header.module.scss';
 import c from './../FavouritesBtn/FavBtn.module.scss';
 import cart from './../../../assets/header/icons/cart.svg';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { useAppSelector } from '../../../redux/hooks';
-import { selectIsAuth } from '../../../redux/authSlice';
 
 interface ICartBtnProps {
     fullHeader: boolean
 }
 
 export const CartBtn: FC = ({ fullHeader }:ICartBtnProps) => {
-    const isAuth = useAppSelector(selectIsAuth)
 
     const userCart = useAppSelector(s => s.auth.loginData.data?.cart)
     let userCartlength = userCart?.length
 
     const userCartlenInLS = useAppSelector(s => s.products.cartInLSLength)
-    //let userCartlenInLS = JSON.parse(localStorage.getItem('cart'))?.length 
-    
-   
-    useEffect( () => {
-        if (!isAuth) {
-            let userCartlenInLS = JSON.parse(localStorage.getItem('cart'))?.length 
-            
-        }
-    }, [isAuth])
                            
 
     return <div className={c.menuItem} style={fullHeader ? null : { border: 'none' }}>
